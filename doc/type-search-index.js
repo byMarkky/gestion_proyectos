261bf894ddb723ad8ec46bdd2142a7da89feace2
byMarkky <mo6368745@gmail.com>
2024-04-02 10:08:41 +0200

    Fix doc

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"gestionproyectos","l":"Equipo"},{"p":"gestionproyectos","l":"GestionProyectos"},{"p":"gestionproyectos","l":"Main"},{"p":"gestionproyectos","l":"Proyecto"}];updateSearchResults();
